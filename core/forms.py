1aa145a678ed33a1edfe70eeaf3b849219d97702
ivan <mr.ivan.kostenko@mail.ru>
2023-02-01 01:48:10 +0300

    new personalinfo input

--- core/forms.py
+++ core/forms.py
@@ -11,4 +11,17 @@ class CommentForm(forms.ModelForm):
 class PersonalInfoForm(forms.ModelForm):
     class Meta:
         model = PersonalInfo
-        fields = '__all__'
+        fields = ['user', 'name', 'surname', 'mobile', 'instagram', 'mass', 'height', 'email', 'sex', 'birthdate', 'level']
+
+    def __init__(self, *args, **kwargs):
+        super(PersonalInfoForm, self).__init__(*args, **kwargs)
+        self.fields['name'].required = False
+        self.fields['surname'].required = False
+        self.fields['mobile'].required = False
+        self.fields['instagram'].required = False
+        self.fields['mass'].required = False
+        self.fields['height'].required = False
+        self.fields['email'].required = False
+        self.fields['sex'].required = False
+        self.fields['birthdate'].required = False
+        self.fields['level'].required = False

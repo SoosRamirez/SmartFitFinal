82d83899e5fda129595617e7dbf48683d505921a
SoosRamirez <81272167+SoosRamirez@users.noreply.github.com>
2023-02-03 00:24:15 +0300

    modal fixed

--- core/views.py
+++ core/views.py
@@ -286,18 +286,18 @@ def subscribe(request, program_id):
 @csrf_protect
 def purchase(request):
     template = 'purchase.html'
-    payment = Payment.create({
-        "amount": {
-            "value": "100.00",
-            "currency": "RUB"
-        },
-        "confirmation": {
-            "type": "redirect",
-            "return_url": "https://www.example.com/return_url"
-        },
-        "capture": True,
-        "description": "Заказ №1"
-    }, uuid.uuid4())
+    # payment = Payment.create({
+    #     "amount": {
+    #         "value": "100.00",
+    #         "currency": "RUB"
+    #     },
+    #     "confirmation": {
+    #         "type": "redirect",
+    #         "return_url": "https://www.example.com/return_url"
+    #     },
+    #     "capture": True,
+    #     "description": "Заказ №1"
+    # }, uuid.uuid4())
     context = {
         'reviews': Feedback.objects.all(),
         'questions': Question.objects.all()
